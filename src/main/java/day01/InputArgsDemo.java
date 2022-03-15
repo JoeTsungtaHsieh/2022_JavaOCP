0778fa4c4182bbf2d0e52d457e30828c59d77520
MB-study <MB-study@MB-205-7>
2022-03-15 20:05:36 +0800

    ocp

---
+++ src/main/java/day01/InputArgsDemo.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class InputArgsDemo {
+
+    public static void main(String[] args) {
+        System.out.println("第一個得到的參數內容:" + args[0]);
+        System.out.printf("第一個得到的參數內容: %s\n", args[1]);
+    }
+
+}
---
+++ src/main/java/day01/LoopDemo.java
@@ -0,0 +1,14 @@
+package day01;
+// https://github.com/JoeTsungtaHsieh/2022_JavaOCP.git
+// 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
+public class LoopDemo {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1;i<=10;i++){
+            System.out.printf("i = %d\n",i);
+            sum+= i;
+            System.out.printf("sum = %d\n", sum);
+        }
+        System.out.printf("total sum = %d\n", sum);
+    }
+}
