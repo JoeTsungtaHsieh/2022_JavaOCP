2cf8362bd03efcf7e51d8479b1a7ac374cc53d13
MB-study <MB-study@MB-205-7>
2022-03-29 19:29:56 +0800

    ocp

---
+++ src/main/java/day05_oo/Hambuger.java
@@ -0,0 +1,10 @@
+package day05_oo;
+
+public class Hambuger {
+    private String name; // 漢堡名稱
+    private Bread bread;  // 內容物-麵包
+    private Cheese cheese; // 內容物-起司
+    private Beef beef;   // 內容物-牛肉
+    private Integer price; // 漢堡基礎價
+    
+}
