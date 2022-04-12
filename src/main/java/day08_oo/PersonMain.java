562d3a370b9c17e19bdeb5b1dba622afacabdc03
MB-study <MB-study@MB-205-7>
2022-04-12 20:03:09 +0800

    ocp

---
+++ src/main/java/day08_oo/PersonMain.java
@@ -0,0 +1,10 @@
+package day08_oo;
+
+
+public class PersonMain {
+    public static void main(String[] args) {
+        Student student = new Student("John", 18, 100);
+        Teacher teacher = new Teacher("Mary", 28, 50000);
+        
+    }
+}
