7a98e259eb61adb040ad9aeccb0b6440d79851e3
MB-study <MB-study@MB-205-7>
2022-04-12 19:57:33 +0800

    ocp

---
+++ src/main/java/day08_oo/Teacher.java
@@ -0,0 +1,15 @@
+package day08_oo;
+
+public class Teacher extends Person{
+    private int salary;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    
+    
+}
