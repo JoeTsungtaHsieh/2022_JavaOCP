155b765c5a7e622d7c82f4542a30cdbb32a6c688
MB-study <MB-study@MB-205-7>
2022-04-14 21:09:08 +0800

    ocp

--- src/main/java/day09_oo2/BMW.java
+++ src/main/java/day09_oo2/BMW.java
@@ -1,21 +1,21 @@
 package day09_oo2;
 
-public class Benz implements Car{
+public class BMW implements Car {
+
     private int power;
 
-    public Benz(int power) {
+    public BMW(int power) {
         this.power = power;
     }
-    
-    
+
     @Override
     public void move() {
         System.out.println("用車輪走");
     }
-    
+
     @Override
     public int getPower() {
         return power;
     }
-    
+
 }
--- src/main/java/day09_oo2/Car.java
+++ src/main/java/day09_oo2/Car.java
@@ -2,5 +2,5 @@ package day09_oo2;
 
 public interface Car {
     void move(); // 沒有實作{}的方法統稱為抽象(abstract)方法
-    int power();
+    int getPower();
 }
