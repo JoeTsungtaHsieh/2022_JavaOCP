4450163506cac7d3b0e659a0c3ce6e273dbbcbdd
MB-study <MB-study@MB-205-7>
2022-04-21 20:50:38 +0800

    ocp

---
+++ src/main/java/day11_inner3/ButtonMain.java
@@ -0,0 +1,30 @@
+package day11_inner3;
+
+
+public class ButtonMain {
+    public static void main(String[] args) {
+        Button manulButton = new ManulButton();
+        manulButton.action();
+        
+        
+        Button autoButton = new AutoButton();
+        autoButton.action();
+        
+        Button yoyoButton = new Button() {
+            @Override
+            public void action() {
+                System.out.println("悠遊卡登入");
+            }
+        };
+        
+        Button creditCardButton = new Button() {
+            @Override
+            public void action() {
+                System.out.println("信用卡登入");
+            }
+        };
+        
+        yoyoButton.action();
+        creditCardButton.action();
+    }
+}
