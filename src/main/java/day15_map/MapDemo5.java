4a07739512a71fe57fdafab488f11f8a6d593249
MB-study <MB-study@MB-205-7>
2022-05-05 21:44:54 +0800

    ocp

--- src/main/java/day15_map/MapDemo5.java
+++ src/main/java/day15_map/MapDemo5.java
@@ -14,12 +14,15 @@ public class MapDemo5 {
         people.put("Jo", 17);
         System.out.println(people);
         //----------------------------------------------
+        Fruit.order = Order.PRICE;
+        // 注意: 若有相同價格,只會出現一個水果元素
+        // 例如 apple 與 papaya都是 1.99 ,則 fruits 集合中只會出現 apple
         TreeMap<Fruit, String> fruits = new TreeMap<>();
         fruits.put(new Fruit("apple", 100, 1.99),"梨山");
-        fruits.put(new Fruit("watermelon", 100, 1.99),"花蓮");
-        fruits.put(new Fruit("banana", 100, 1.99),"屏東");
-        fruits.put(new Fruit("papaya", 100, 1.99),"南投");
-        
+        fruits.put(new Fruit("watermelon", 200, 3.99),"花蓮");
+        fruits.put(new Fruit("banana", 300, 2.99),"屏東");
+        fruits.put(new Fruit("papaya", 400, 1.99),"南投");
+        System.out.println(fruits);
     }
 
 }
