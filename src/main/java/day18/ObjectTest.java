5917b62451fabcec54745ba500ec6ab9779180ce
MB-study <MB-study@MB-205-7>
2022-05-17 21:42:19 +0800

    ocp

--- src/main/java/day18/ObjectTest.java
+++ src/main/java/day18/ObjectTest.java
@@ -4,22 +4,34 @@ class BigData {
     private String data;
 
     BigData() {
+        System.out.printf("建構子執行者: %s\n",Thread.currentThread().getName());
         for(int i=0; i<=9999;i++){
             data += String.valueOf(i);
         }
+
     }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize(); //To change body of generated methods, choose Tools | Templates.
+        System.out.printf("finalize 執行者: %s\n",Thread.currentThread().getName());
+    
+    }
+    
+    
+    
 } 
     public class ObjectTest {
         public static void main(String[] args)throws Exception {
-            System.gc();
+            System.out.printf("main方法執行者: %s\n",Thread.currentThread().getName());
             Runtime runtime = Runtime.getRuntime();
             System.out.printf("FreeMemory: %,d bytes\n",runtime.freeMemory());
             BigData bigData = new BigData();
-            Thread.sleep(1000);
+            Thread.sleep(1000); // 等待一秒鐘
             System.out.printf("FreeMemory: %,d bytes\n",runtime.freeMemory());
             bigData = null;
             System.gc();
-            Thread.sleep(1000);
+            Thread.sleep(1000); // 等待一秒鐘
             System.out.printf("FreeMemory: %,d bytes\n",runtime.freeMemory());
             
         }
